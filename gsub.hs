f2c8ada5b7f70ee591ce3de95fe46b20828ad637
ujihisa <ujihisa@gmail.com>
2011-06-14 18:04:04 -0700

    from https://gist.github.com/1024144

---
+++ gsub.hs
@@ -0,0 +1,22 @@
+module Regex where
+
+import Text.Regex.Posix
+
+-------------------------------------------------------------------------------
+gsub :: String -> String -> String -> String
+gsub text match replacement = replaceMatches 0 matches text
+  where
+    matches = getAllMatches 
+      (text =~ match :: AllMatches [] (MatchOffset, MatchLength))
+    rl = length replacement
+    replaceMatches _ [] text = text
+    replaceMatches accum ((o, l):ms) text = replaceMatches accum' ms text'
+      where
+      (pre, post) = splitAt (o + accum) text
+      accum' = accum + (rl - l)
+      post' = drop l post
+      text' = pre ++ replacement ++ post'
+
+main :: IO ()
+main = print (gsub "roman andre gonzalez urdaneta" "[ng]" "_")
+
